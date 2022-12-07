de80f98657ee08afcb349e8d9f5cec046ee6b2f7
shailja-thakur <s7thakur@uwaterloo.ca>
2022-12-07 17:00:19 -0500

    python script for ChatGPT

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 import json
 import os
 import time
-
+import re
 # Local
 from Classes import auth as Auth
 from Classes import chat as Chat
@@ -60,52 +60,52 @@ def start_chat():
     previous_convo_id = None
     conversation_id = None
     access_token = Auth.get_access_token()
-    while True:
-        try:
-            if access_token == "":
-                print(f"{Fore.RED}>> Access token is missing in /Classes/auth.json.")
-                exit(1)
-
-            user_input = input("You: ")
-
-            
-
-            answer, previous_convo, convo_id = Chat.ask(auth_token=access_token,
-                                                        prompt=user_input,
-                                                        conversation_id=conversation_id,
-                                                        previous_convo_id=previous_convo_id)
-
-            # Extract code snippet from the answer
-            regex = re.compile(r"```(.+?)```", re.S)
-
-            match = regex.findall(answer)
-            
-            # print(match)
-            code=""
-            for m in match:
-                # print(m.group(1))
-                # print(m)
-                code+=m
-
-
-            if answer == "400" or answer == "401":
-                print(f"{Fore.RED}>> Your token is invalid. Attempting to refresh..")
-                open_ai_auth = Auth.OpenAIAuth(email_address=email, password=password)
-                open_ai_auth.begin()
-                time.sleep(3)
-                access_token = Auth.get_access_token()
-            else:
-                if previous_convo is not None:
-                    previous_convo_id = previous_convo
-
-                if convo_id is not None:
-                    conversation_id = convo_id
-
-                if bool(code):
-                    print(f"Chat GPT: {code}")
-        except KeyboardInterrupt:
-            print(f"{Fore.RED}>> Exiting...")
+    
+    try:
+        if access_token == "":
+            print(f"{Fore.RED}>> Access token is missing in /Classes/auth.json.")
             exit(1)
+
+        user_input = input("You: ")
+
+        
+
+        answer, previous_convo, convo_id = Chat.ask(auth_token=access_token,
+                                                    prompt=user_input,
+                                                    conversation_id=conversation_id,
+                                                    previous_convo_id=previous_convo_id)
+
+        # Extract code snippet from the answer
+        regex = re.compile(r"```(.+?)```", re.S)
+
+        match = regex.findall(answer)
+        
+        # print(match)
+        code=""
+        for m in match:
+            # print(m.group(1))
+            # print(m)
+            code+=m
+
+
+        if answer == "400" or answer == "401":
+            print(f"{Fore.RED}>> Your token is invalid. Attempting to refresh..")
+            open_ai_auth = Auth.OpenAIAuth(email_address=email, password=password)
+            open_ai_auth.begin()
+            time.sleep(3)
+            access_token = Auth.get_access_token()
+        else:
+            if previous_convo is not None:
+                previous_convo_id = previous_convo
+
+            if convo_id is not None:
+                conversation_id = convo_id
+
+            if bool(code):
+                print(f"Chat GPT: {code}")
+    except KeyboardInterrupt:
+        print(f"{Fore.RED}>> Exiting...")
+        exit(1)
 
 
 if __name__ == "__main__":
